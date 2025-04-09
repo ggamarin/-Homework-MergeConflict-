1b0645803bda52d77cdc9102c7c790177c6d90cb
Grigoriy Gamarin <gam110191@gmail.com>
2025-04-09 23:26:19 +0300

    test

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.yahoo.com/");
+        open("https://www.bing.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
